4f2b1d7b67a683c891409e17f4ea97df9b7187c3
Nicolas Dubois <nicolas.dubois@student.isae-supaero.fr>
2026-09-24 21:09:50 +0200

    Correction numeric intergration

--- python/Naive_Algorithm/integrateur_numerique.py
+++ python/Naive_Algorithm/integrateur_numerique.py
@@ -11,17 +11,12 @@ def mettre_a_jour_position_et_vitesse(systeme):
     # Calcul de la position suivante d'une particule
     def position_suivante_un_objet(particule, dt):
         # Calcul des positions suivantes
-        x_next = particule.position[0] + particule.velocity[0] * dt + (1/2) * particule.force[0] * (dt**2)
-        y_next = particule.position[1] + particule.velocity[1] * dt + (1/2) * particule.force[1] * (dt**2)
-        z_next = particule.position[2] + particule.velocity[2] * dt + (1/2) * particule.force[2] * (dt**2)
+        acceleration = particule.force / particule.mass
         # Calcul des vitesses suivantes
-        vx_next = (x_next - particule.position[0]) / dt
-        vy_next = (y_next - particule.position[1]) / dt
-        vz_next = (z_next - particule.position[2]) / dt
+        particule.velocity += acceleration * dt
         # Modification des données
-        particule.position = np.array([x_next, y_next, z_next])
-        particule.velocity = np.array([vx_next, vy_next, vz_next])
-    # Application à chaque particule
+        particule.position += particule.velocity * dt
+        # Application à chaque particule
     for particule in systeme.particles :
         position_suivante_un_objet(particule, dt)
     return
